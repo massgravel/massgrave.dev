cbc38b552ff1bf4c2859b6f18a31a26b5402368f
WindowsAddict <windowsaddict@protonmail.com>
2022-05-20 21:07:29 +0530

    Test update

--- get-mas.ps1
+++ get-mas.ps1
@@ -1,34 +1,7 @@
-function Exit-Script {
+$Tls12 = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072); [System.Net.ServicePointManager]::SecurityProtocol = $Tls12;
+$DownloadUrl = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_1.5_AIO_CRC32_21D20776.cmd'
+$FilePath = "$env:USERPROFILE\Desktop\MAS_1.5_AIO_CRC32_21D20776.cmd"
 
-    [CmdletBinding()]
-    param(
-        [Parameter(Position = 0)]
-        [int]
-        $ExitCode = 0
-    )
+(New-Object System.Net.WebClient).DownloadFile($DownloadUrl, $FilePath)
 
-    process {
-        if (Test-Path -Path "$env:TEMP\MAS.cmd") {
-            Remove-Item -Path "$env:TEMP\MAS.cmd" -Force
-        }
-
-        exit $ExitCode
-    }
-
-}
-
-# Enable TLSv1.2 and TLSv1.3 for compatibility with older clients
-$TLS12Protocol = [System.Net.SecurityProtocolType] 'Tls12', 'Tls13'
-[System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
-
-$DownloadURL = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_1.5_AIO_CRC32_21D20776.cmd'
-
-try {
-    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile "$env:TEMP\MAS.cmd"
-} catch {
-    Write-Error $_
-    Exit-Script -ExitCode 1
-}
-
-Start-Process -FilePath "$env:TEMP\MAS.cmd" -Wait
-Exit-Script
+Start-Process -FilePath $FilePath
