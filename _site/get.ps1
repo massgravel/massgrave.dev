285226ca354f2638b49edc5b4bec198723f7fe7d
DESKTOP-RBH716R\Admin <windowsaddict@protonmail.com>
2022-07-18 01:42:01 +0530

    code update

--- _site/get.ps1
+++ _site/get.ps1
@@ -1,7 +1,15 @@
-$Tls12 = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072); [System.Net.ServicePointManager]::SecurityProtocol = $Tls12;
+# Enable TLSv1.2 and TLSv1.3 for compatibility with older clients
+$TLS12Protocol = [System.Net.SecurityProtocolType] 'Tls12', 'Tls13'
+[System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
+
+
 $DownloadUrl = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_1.5_AIO_CRC32_21D20776.cmd'
 $FilePath = "$env:USERPROFILE\Desktop\MAS_1.5_AIO_CRC32_21D20776.cmd"
 
+if($PSVersionTable.PSVersion.Major -lt 3){
 (New-Object System.Net.WebClient).DownloadFile($DownloadUrl, $FilePath)
+}else {
+Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+}
 
 Start-Process -FilePath $FilePath
