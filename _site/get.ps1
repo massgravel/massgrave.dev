1980e549ef668470e7b5d803808bfd1cd9794220
DESKTOP-RBH716R\Admin <windowsaddict@protonmail.com>
2022-07-25 22:52:59 +0530

    test

--- _site/get.ps1
+++ _site/get.ps1
@@ -2,7 +2,7 @@ $Tls12 = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072); [System.Net.
 
 $DownloadUrl = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_1.5_AIO_CRC32_21D20776.cmd'
 $FilePath = "$env:USERPROFILE\Desktop\MAS_1.5_AIO_CRC32_21D20776.cmd"
-
+ 
 if($PSVersionTable.PSVersion.Major -lt 3){
 (New-Object System.Net.WebClient).DownloadFile($DownloadUrl, $FilePath)
 }else {
