5562d0432a6ecffda134e29ce1a8993fb86e2a50
DESKTOP-V026VQF\Home <windowsaddict@protonmail.com>
2023-03-11 07:17:37 +0530

    change hash on file creation

--- _site/get.ps1
+++ _site/get.ps1
@@ -4,19 +4,25 @@
 $DownloadURL = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd'
 $DownloadURL2 = 'https://gitlab.com/massgrave/microsoft-activation-scripts/-/raw/master/MAS/All-In-One-Version/MAS_AIO.cmd'
 
-$FilePath = "$env:TEMP\MAS.cmd"
-$ScriptArgs = "$args "
+$rand = Get-Random -Maximum 1000
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
 
 try {
-    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath -ErrorAction Stop
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -ErrorAction Stop
 } catch {
     try {
-        Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing -OutFile $FilePath -ErrorAction Stop
+        $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing -ErrorAction Stop
     } catch {
-    Write-Error $_
-  Return
+        Write-Error $_
+        Return
+    }
 }
-}
+
+$ScriptArgs = "$args "
+$prefix = "REM $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
 
 if (Test-Path $FilePath) {
     Start-Process $FilePath $ScriptArgs -Wait
