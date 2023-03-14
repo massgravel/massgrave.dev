e3a22f7c7ad31e9b3a5d93095930a615f7b4972f
DESKTOP-V026VQF\Home <windowsaddict@protonmail.com>
2023-03-14 17:01:00 +0530

    Update

--- _site/get.ps1
+++ _site/get.ps1
@@ -1,5 +1,6 @@
+$ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 $DownloadURL = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd'
 $DownloadURL2 = 'https://gitlab.com/massgrave/microsoft-activation-scripts/-/raw/master/MAS/All-In-One-Version/MAS_AIO.cmd'
@@ -9,23 +10,17 @@ $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -mat
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
 
 try {
-    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -ErrorAction Stop
-} catch {
-    try {
-        $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing -ErrorAction Stop
-    } catch {
-        Write-Error $_
-        Return
-    }
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
 }
 
 $ScriptArgs = "$args "
-$prefix = "REM $rand `r`n"
+$prefix = "@REM $rand `r`n"
 $content = $prefix + $response
 Set-Content -Path $FilePath -Value $content
 
-if (Test-Path $FilePath) {
-    Start-Process $FilePath $ScriptArgs -Wait
-    $item = Get-Item -LiteralPath $FilePath
-    $item.Delete()
-}
+Start-Process $FilePath $ScriptArgs -Wait
+$item = Get-Item -LiteralPath $FilePath
+$item.Delete()
