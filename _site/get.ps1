55bcd8f4d7e79bb5b8c61b134a2d521272d3108d
WindowsAddict <windowsaddict@protonmail.com>
2024-02-14 11:45:10 +0530

    Change file hash

--- _site/get.ps1
+++ _site/get.ps1
@@ -1,4 +1,4 @@
-# Check the instructions here on how to use it https://massgrave.dev/
+# Check the instructions here on how to use it mass grave[.]dev
 
 $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
