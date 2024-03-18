e466ef6be7a8a2a80e066145abe5e3c401aea6bb
WindowsAddict <windowsaddict@protonmail.com>
2024-03-18 13:14:33 +0530

    Randomize download URL

--- get.ps1
+++ get.ps1
@@ -4,18 +4,22 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO-CRC32_9AE8AFBA.cmd'
-$DownloadURL2 = 'https://bitbucket.org/WindowsAddict/microsoft-activation-scripts/raw/master/MAS/All-In-One-Version/MAS_AIO-CRC32_9AE8AFBA.cmd'
+$CommonURLPart = 'MAS/All-In-One-Version/MAS_AIO-CRC32_9AE8AFBA.cmd'
+$DownloadURL1 = 'https://bitbucket.org/WindowsAddict/microsoft-activation-scripts/raw/master/' + $CommonURLPart
+$DownloadURL2 = 'https://codeberg.org/massgravel/Microsoft-Activation-Scripts/raw/branch/master/' + $CommonURLPart
+$DownloadURL3 = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/' + $CommonURLPart
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
 
+$RandomURL = Get-Random -InputObject $DownloadURL1, $DownloadURL2
+
 try {
-    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+    $response = Invoke-WebRequest -Uri $RandomURL -UseBasicParsing
 }
 catch {
-    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+    $response = Invoke-WebRequest -Uri $DownloadURL3 -UseBasicParsing
 }
 
 $ScriptArgs = "$args "
