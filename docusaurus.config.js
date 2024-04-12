fae7e4be449e32035cdbadebfdb70409420094ee
WindowsAddict <windowsaddict@protonmail.com>
2024-04-13 02:07:26 +0530

    Add "trailingSlash: false,"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,7 @@ const config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
