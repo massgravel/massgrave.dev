437bde386b1b04aeb3b920fbc1a48d1991d6ef11
WindowsAddict <windowsaddict@protonmail.com>
2024-06-09 06:17:48 +0530

    Update discord banner

--- src/components/DiscordBadge.js
+++ src/components/DiscordBadge.js
@@ -3,7 +3,7 @@ import React from 'react';
 function DiscordBadge() {
   return (
     <a href="https://discord.gg/tVFN4N84PP">
-      <img height="40px" src="https://img.shields.io/discord/746721520931569757?style=social&logo=discord&label=Chat%20with%20us" alt="Discord" />
+      <img height="40px" src="https://img.shields.io/badge/Chat%20with%20us%20on%20Discord--blue?style=social&logo=discord" alt="Discord" />
     </a>
   );
 }
