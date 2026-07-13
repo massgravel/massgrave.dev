2a7f49c3fbc22d5bea86a35bcce32bf2a4be1a34
Lyssa <75037904+thecatontheceiling@users.noreply.github.com>
2026-07-13 16:49:53 +0400

    Use a different font for readability

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,12 @@ const config = {
     ],
   ],
 
+  stylesheets: [
+    {
+      href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
+    }
+  ],
+
   plugins: [
     [
       '@docusaurus/plugin-client-redirects',
